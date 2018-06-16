872071fc6c569e20e75208111db2edecef5f8e9a
injkgz <injecti0n.kg@gmail.com>
2018-06-16 17:58:34 +0700

    Исправление DiscountFactoryTest.cs

---
+++ Discount.Tests/DiscountFactoryTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Discount.Tests
+{
+    [TestFixture]
+    public class DiscountFactoryTest
+    {
+    [Test]
+    [TestCase(Discounts.Coupon, 11, TestName = "Проверка корректного возвращения скидки типа Coupon")]
+    [TestCase(Discounts.Percent, 11, TestName = "Проверка корректного возвращения скидки типа Percent")]
+    public void GetDiscount(Discounts key,
+        double value)
+    {
+        switch (key)
+        {
+            case Discounts.Coupon:
+            {
+                var discount = DiscountFactory.GetDiscount(key, 11);
+                var discountResult = discount as CouponDiscount;
+                Assert.AreSame(discount, discountResult);
+                break;
+            }
+            case Discounts.Percent:
+            {
+                var discount = DiscountFactory.GetDiscount(key, 11);
+                var discountResult = discount as PercentDiscount;
+                Assert.AreSame(discount, discountResult);
+                break;
+            }
+        }
+    }
+    }
+}
