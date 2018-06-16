8a309f2943942563feb4b8bd57a4d413ca308c67
injkgz <injecti0n.kg@gmail.com>
2018-06-16 18:11:49 +0700

    Изменил название проекта.

--- Discount.Tests/DiscountFactoryTest.cs
+++ Discount.Tests/DiscountFactoryTest.cs
@@ -1,34 +1,33 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace Discount.Tests
 {
     [TestFixture]
     public class DiscountFactoryTest
     {
-    [Test]
-    [TestCase(Discounts.Coupon, 11, TestName = "Проверка корректного возвращения скидки типа Coupon")]
-    [TestCase(Discounts.Percent, 11, TestName = "Проверка корректного возвращения скидки типа Percent")]
-    public void GetDiscount(Discounts key,
-        double value)
-    {
-        switch (key)
+        [Test]
+        [TestCase(Discounts.Coupon, 11, TestName = "Проверка корректного возвращения скидки типа Coupon")]
+        [TestCase(Discounts.Percent, 11, TestName = "Проверка корректного возвращения скидки типа Percent")]
+        public void GetDiscount(Discounts key,
+            double value)
         {
-            case Discounts.Coupon:
+            switch (key)
             {
-                var discount = DiscountFactory.GetDiscount(key, 11);
-                var discountResult = discount as CouponDiscount;
-                Assert.AreSame(discount, discountResult);
-                break;
-            }
-            case Discounts.Percent:
-            {
-                var discount = DiscountFactory.GetDiscount(key, 11);
-                var discountResult = discount as PercentDiscount;
-                Assert.AreSame(discount, discountResult);
-                break;
+                case Discounts.Coupon:
+                {
+                    var discount = DiscountFactory.GetDiscount(key, 11);
+                    var discountResult = discount as CouponDiscount;
+                    Assert.AreSame(discount, discountResult);
+                    break;
+                }
+                case Discounts.Percent:
+                {
+                    var discount = DiscountFactory.GetDiscount(key, 11);
+                    var discountResult = discount as PercentDiscount;
+                    Assert.AreSame(discount, discountResult);
+                    break;
+                }
             }
         }
-    }
     }
 }
